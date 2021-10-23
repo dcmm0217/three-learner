7c2b8957bb8f8abdc140ff34d0085b823d869a3c
Shiotsuki_ <564165781@qq.com>
2021-10-23 12:34:43 +0800

    feat：开发环境准备

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  hellow three-learner
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+
+<style>
+#app {
+
+}
+</style>
