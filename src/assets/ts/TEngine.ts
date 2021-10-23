ab0df786bcfbdd892ffa8bd9b3330bf1b291d9c2
Shiotsuki_ <564165781@qq.com>
2021-10-23 14:27:53 +0800

    feat：渲染立方体

--- src/assets/ts/TEngine.ts
+++ src/assets/ts/TEngine.ts
@@ -1,15 +1,35 @@
-import { WebGLRenderer } from "three"
+import { BoxBufferGeometry, Mesh, MeshStandardMaterial, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from "three"
 
 export class TEngine {
 
   private dom: HTMLElement
   private renderer: WebGLRenderer
 
+  private scene: Scene
+  private camera: PerspectiveCamera
+
   constructor (dom: HTMLElement) {
     this.dom = dom
     this.renderer = new WebGLRenderer()
-    console.log(dom)
+    this.scene = new Scene()
+    this.camera = new PerspectiveCamera(45, dom.offsetWidth / dom.offsetHeight, 1, 1000)
+
+    this.camera.position.set(20, 20 ,20)
+    this.camera.lookAt(new Vector3(0, 0, 0))
+    this.camera.up = new Vector3(0, 1, 0)
+
     dom.appendChild(this.renderer.domElement)
     this.renderer.setSize(dom.offsetWidth, dom.offsetHeight, true)
+
+    const box: Mesh = new Mesh(
+      new BoxBufferGeometry(10, 10, 10),
+      new MeshStandardMaterial()
+    )
+
+    this.scene.add(box)
+
+    this.renderer.setClearColor('rgb(255, 255, 255)')
+    this.renderer.clearColor()
+    this.renderer.render(this.scene, this.camera)
   }
 }
