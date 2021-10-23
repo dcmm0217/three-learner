5a9ee7af2e4ebb7d81c3bbed06c9584c2d1b27c4
Shiotsuki_ <564165781@qq.com>
2021-10-23 13:46:24 +0800

    feat：three与vue对接

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
-  hellow three-learner
+  <div class="three-canvas" ref="threeTarget"></div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
+import { TEngine} from './assets/ts/TEngine'
 
 export default defineComponent({
   setup() {
-    
+    const threeTarget = ref(null)
+
+    onMounted(() => {
+      const TE = new TEngine(threeTarget.value!)
+    })
+
+    return {
+      threeTarget
+    }
   },
 })
 </script>
 
 <style>
+.three-canvas {
+  width: 100%;
+  height: 100%;
+}
 #app {
-
+  width: 100%;
+  height: 100%;
 }
 </style>
