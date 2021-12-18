5dfe97dac916ae54b69c6f48061f7c64868f950c
Shiotsuki_ <564165781@qq.com>
2021-12-18 17:11:07 +0800

    feat：物体拾取与变换控制器

--- src/assets/ts/TEngine.ts
+++ src/assets/ts/TEngine.ts
@@ -10,28 +10,36 @@ import {
   Vector3,
   WebGLRenderer,
   MOUSE, 
-  Object3D} from "three"
+  Object3D,
+  Vector2,
+  Raycaster} from "three"
 
   import Stats from 'three/examples/jsm/libs/stats.module'
   import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+  import { TransformControls } from 'three/examples/jsm/controls/TransformControls'
 
 export class TEngine {
 
   private dom: HTMLElement
   private renderer: WebGLRenderer
+  private transformControls: TransformControls
+
+  private raycaster: Raycaster
+
+  private mouse: Vector2
 
   private scene: Scene
   private camera: PerspectiveCamera
 
   constructor (dom: HTMLElement) {
     this.dom = dom
-    this.renderer = new WebGLRenderer({
+    const renderer = new WebGLRenderer({
       antialias: true
     })
 
-    this.renderer.shadowMap.enabled = true
+    renderer.shadowMap.enabled = true
 
-    this.scene = new Scene()
+    const scene = new Scene()
     this.camera = new PerspectiveCamera(45, dom.offsetWidth / dom.offsetHeight, 1, 1000)
 
     this.camera.position.set(200, 200 ,200)
@@ -39,7 +47,7 @@ export class TEngine {
     this.camera.up = new Vector3(0, 1, 0)
 
     
-    this.renderer.setSize(dom.offsetWidth, dom.offsetHeight, true)
+    renderer.setSize(dom.offsetWidth, dom.offsetHeight, true)
     
     // 初始性能监视器
     const stats = Stats()
@@ -50,25 +58,63 @@ export class TEngine {
     statsDom.style.left = 'unset'
 
     // 初始orbitControls
-    const orbitControls: OrbitControls = new OrbitControls(this.camera, this.renderer.domElement)
+    const orbitControls: OrbitControls = new OrbitControls(this.camera, renderer.domElement)
     orbitControls.mouseButtons = {
       LEFT: null as unknown as MOUSE,
       MIDDLE: MOUSE.DOLLY,
       RIGHT: MOUSE.ROTATE
     }
 
+    // 初始变换控制器
+    const transformControls = new TransformControls(this.camera, renderer.domElement)
+    scene.add(transformControls)
+    // 初始射线发射器
+    const raycaster = new Raycaster()
+
+    // 给renderer的canvas对象添加鼠标事件
+    const mouse = new Vector2()
+    let x = 0
+    let y = 0
+    let width = 0
+    let height = 0
+    renderer.domElement.addEventListener('mousemove', (event) => {
+      x = event.offsetX
+      y = event.offsetY
+      width = renderer.domElement.offsetWidth
+      height = renderer.domElement.offsetHeight
+      mouse.x = x / width * 2 - 1
+      mouse.y = -y * 2 / height + 1
+    })
+
+    renderer.domElement.addEventListener('click', event => {
+      raycaster.setFromCamera(mouse, this.camera)
+
+      const intersection = raycaster.intersectObjects(scene.children)
+
+      if (intersection.length) {
+       const object = intersection[0].object
+       transformControls.attach(object)
+      }
+    })
+
     const renderFun = () => {
       orbitControls.update()
 
-      this.renderer.render(this.scene, this.camera)
+      renderer.render(scene, this.camera)
       stats.update()
       requestAnimationFrame(renderFun)
     }
 
     renderFun()
 
-    dom.appendChild(this.renderer.domElement)
+    dom.appendChild(renderer.domElement)
     dom.appendChild(statsDom)
+
+
+    this.renderer = renderer
+    this.scene = scene
+    this.transformControls = transformControls
+    this.mouse = mouse
   }
 
   addObject (...object: Object3D[]) {
