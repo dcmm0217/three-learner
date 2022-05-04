cdf74d65efb2807cac7a96f3a5721d36b2fec477
Shiotsuki_ <564165781@qq.com>
2022-05-04 15:54:06 +0800

    feat：精灵物体与深度测试

--- src/assets/ts/TGroup.ts
+++ src/assets/ts/TGroup.ts
@@ -1,4 +1,12 @@
-import { Group, Mesh, MeshStandardMaterial, PlaneBufferGeometry } from "three";
+import {
+  Group,
+  Mesh,
+  MeshBasicMaterial,
+  MeshStandardMaterial,
+  PlaneBufferGeometry,
+  Sprite,
+  SpriteMaterial,
+} from "three";
 import { pictureTexture, tipsTexture } from "./TTextures";
 import { framePromise } from "./TLoadModel";
 
@@ -18,14 +26,28 @@ export const groupPromise = new Promise<Group>((resolve, reject) => {
   group.add(picture);
 
   // 标签
-  const tips: Mesh = new Mesh(
-    new PlaneBufferGeometry(16, 9),
-    new MeshStandardMaterial({
+  // const tips: Mesh = new Mesh(
+  //   new PlaneBufferGeometry(16, 9),
+  //   new MeshBasicMaterial({
+  //     map: tipsTexture,
+  //   })
+  // );
+
+  const tips: Sprite = new Sprite(
+    new SpriteMaterial({
       map: tipsTexture,
+      // sizeAttenuation: false,
+      // depthWrite: false,
+      // depthTest: false,
     })
   );
 
-  tips.position.set(0, -25, 0);
+  tips.position.set(0, 45, 0);
+  tips.scale.set(16, 9, 1);
+
+  // tips.onBeforeRender = (render, scene, camera) => {
+  //   tips.lookAt(camera.position);
+  // };
 
   group.add(tips);
 
@@ -34,7 +56,7 @@ export const groupPromise = new Promise<Group>((resolve, reject) => {
       // 相框
       group.add(frame);
 
-      group.position.y = 120;
+      group.position.y = 140;
       group.position.z = -70;
 
       group.scale.set(2, 2, 2);
